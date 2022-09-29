916ff8384e553dccca4b213e725f7b6c1f7c70ef
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-09-29 13:00:29 +0800

    Add exception handler for AdoptiumApi.cs

--- AdoptiumApi.cs
+++ AdoptiumApi.cs
@@ -1,12 +1,16 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security;
 using System.Security.Permissions;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization.NodeTypeResolvers;
 using static System.Net.WebRequestMethods;
 
 namespace GraphicalMirai
@@ -21,7 +25,28 @@ namespace GraphicalMirai
         public int tip_version;
     }
 
-    public class ReleaseVersionsParams
+    public class ReleaseVersionsParams : VersionsParams
+    {
+        public string? version;
+
+        public override string ToString()
+        {
+            string s = base.ToString();
+            if (version != null) s += ((s.Length > 0 ? "&" : "?")) + "version=" + UrlEncoder.Default.Encode(version);
+            return s;
+        }
+    }
+    public class AssetVersionsParams : VersionsParams
+    {
+        public string version;
+        public AssetVersionsParams(string version) { this.version = version; }
+
+        public override string ToString()
+        {
+            return "/" + version + base.ToString();
+        }
+    }
+    public class VersionsParams
     {
         public enum Architecture
         {
@@ -71,11 +96,24 @@ namespace GraphicalMirai
         public ReleaseType? release_type;
         public SortMethod? sort_method;
         public SortOrder? sort_order;
-        public string? version;
-
+        public void CopyFrom(VersionsParams v)
+        {
+            architecture = v.architecture;
+            c_lib = v.c_lib;
+            heap_size = v.heap_size;
+            image_type = v.image_type;
+            lts = v.lts;
+            os = v.os;
+            page = v.page;
+            page_size = v.page_size;
+            project = v.project;
+            release_type = v.release_type;
+            sort_method = v.sort_method;
+            sort_order = v.sort_order;
+        }
         public override string ToString()
         {
-            Dictionary<string, string> @params = new ();
+            Dictionary<string, string> @params = new();
             if (architecture.HasValue) @params.Add("architecture", architecture.Value.ToString());
             if (c_lib.HasValue) @params.Add("c_lib", c_lib.Value.ToString());
             if (heap_size.HasValue) @params.Add("heap_size", heap_size.Value.ToString());
@@ -88,8 +126,7 @@ namespace GraphicalMirai
             if (release_type.HasValue) @params.Add("release_type", release_type.Value.ToString());
             if (sort_method.HasValue) @params.Add("sort_method", sort_method.Value.ToString());
             if (sort_order.HasValue) @params.Add("sort_order", sort_order.Value.ToString());
-            if (version?.Length > 0) @params.Add("version", version);
-            string s = string.Join("&", @params.Select((kv,i) => kv.Key + "=" + UrlEncoder.Default.Encode(kv.Value)));
+            string s = string.Join("&", @params.Select((kv, i) => kv.Key + "=" + UrlEncoder.Default.Encode(kv.Value)));
             return s.Length > 0 ? ("?" + s) : "";
         }
     }
@@ -110,6 +147,60 @@ namespace GraphicalMirai
         public int build;
         public string optional;
     }
+    public class AssetVersions
+    {
+        public List<AssetBinary> binaries;
+        public int download_count;
+        public string release_link;
+        public string release_name;
+        public string release_type;
+        public ReleaseSource source;
+        public string timestamp;
+        public string updated_at;
+        public string vendor;
+        public ReleaseVersionData version_data;
+    }
+    public class AssetBinary
+    {
+        public string architecture;
+        public int download_count;
+        public string heap_size;
+        public string image_type;
+        public string jvm_impl;
+        public string os;
+        public AssetPackage package;
+        public string project;
+        public string scm_ref;
+        public string updated_at;
+    }
+    public class AssetPackage
+    {
+        public string checksum;
+        public string checksum_link;
+        public int download_count;
+        public string link;
+        public string metadata_link;
+        public string name;
+        public string signature_link;
+        public int size;
+    }
+    public class ReleaseVersionData
+    {
+        public int build;
+        public int major;
+        public int minor;
+        public string openjdk_version;
+        public string optional;
+        public string pre;
+        public int security;
+        public string semver;
+    }
+    public class ReleaseSource
+    {
+        public string link;
+        public string nane;
+        public int size;
+    }
     public class AdoptiumApi
     {
         private static readonly Lazy<HttpClient> lazyHttpClient = new(() =>
@@ -127,13 +218,55 @@ namespace GraphicalMirai
         private static HttpClient httpClient => lazyHttpClient.Value;
         public static async Task<AvailableReleases?> GetAvailableReleases()
         {
-            var jsonText = await httpClient.GetStringAsync("info/available_releases");
-            return JsonConvert.DeserializeObject<AvailableReleases>(jsonText);
+            var path = "info/available_releases";
+            AvailableReleases? result = null;
+            var jsonText = await httpClient.GetStringAsync(path, jsonText =>
+            {
+                result = JsonConvert.DeserializeObject<AvailableReleases>(jsonText);
+            }, ex =>
+            {
+                if (ex.StatusCode != System.Net.HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("There is an exception when fetching " + path);
+                    Console.WriteLine(ex);
+                }
+            });
+            return result;
         }
         public static async Task<ReleaseVersions?> GetReleaseVersions(ReleaseVersionsParams? @params)
         {
-            var jsonText = await httpClient.GetStringAsync("info/release_versions" + (@params?.ToString() ?? ""));
-            return JsonConvert.DeserializeObject<ReleaseVersions>(jsonText);
+            var path = "info/release_versions" + (@params?.ToString() ?? "");
+            ReleaseVersions? result = null;
+            await httpClient.GetStringAsync(path, jsonText =>
+            {
+                result = JsonConvert.DeserializeObject<ReleaseVersions>(jsonText);
+            }, ex =>
+            {
+                if (ex.StatusCode != System.Net.HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("There is an exception when fetching " + path);
+                    Console.WriteLine(ex);
+                }
+            });
+            return result;
+        }
+        public static async Task<List<AssetVersions>> GetAssetVersions(AssetVersionsParams @params)
+        {
+            var path = "assets/version" + @params.ToString();
+            List<AssetVersions> result = new();
+            await httpClient.GetStringAsync(path, jsonText =>
+            {
+                var temp = JsonConvert.DeserializeObject<List<AssetVersions>?>(jsonText);
+                if (temp != null) result = temp;
+            }, ex =>
+            {
+                if (ex.StatusCode != System.Net.HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("There is an exception when fetching " + path);
+                    Console.WriteLine(ex);
+                }
+            });
+            return result;
         }
     }
 }
