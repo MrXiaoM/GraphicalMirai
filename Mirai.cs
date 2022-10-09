78c2ff6341f1a1fab908ddab88799fd354f71aba
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-10-09 18:33:26 +0800

    update bridge logic

--- Mirai.cs
+++ Mirai.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Printing;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -40,27 +41,14 @@ namespace GraphicalMirai
             onExited += delegate
             {
                 IsRunning = false;
-                onDataReceived("\u001b[91mmirai-console 已停止运行\u001b[0m");
+                socketServer?.Close();
+                
+                onDataReceived($"\u001b[91mmirai-console 已停止运行 ({Process.ExitCode})\u001b[0m");
             };
         }
         public bool InitMirai(string javaHome, string workingDir, List<string> libraries, string mainClass, string extArgs = "")
         {
             if (IsRunning && !Process.HasExited) return false;
-            bool useBridge = Config.Instance.useBridge;
-            if (useBridge)
-            {
-                socketServer = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                socketPort = Config.Instance.bridgePort;
-                IPEndPoint ipEndPoint = new(IPAddress.Loopback, socketPort);
-                try
-                {
-                    socketServer.Bind(ipEndPoint);
-                }
-                catch (Exception e)
-                {
-                    return false;
-                }
-            }
             JavaHome = javaHome;
             WorkingDir = workingDir;
             Libraries = libraries;
@@ -76,8 +64,7 @@ namespace GraphicalMirai
             psi.StandardInputEncoding = Encoding.UTF8;
             psi.StandardOutputEncoding = Encoding.UTF8;
             psi.CreateNoWindow = true;
-            psi.Arguments = (useBridge ? $"-Dgraphicalmirai.bridge.port={socketPort} " : "") +
-                (extArgs + " -classpath \"" + string.Join(";", Libraries) + "\" " + MainClass).TrimStart();
+            psi.Arguments = (extArgs + " -classpath \"" + string.Join(";", Libraries) + "\" " + MainClass).TrimStart();
             Process.StartInfo = psi;
             Process.EnableRaisingEvents = true;
             Process.Exited += delegate
@@ -90,6 +77,31 @@ namespace GraphicalMirai
 
         public void Start()
         {
+            bool useBridge = Config.Instance.useBridge;
+            if (useBridge)
+            {
+                Process.StartInfo.Arguments = $"-Dgraphicalmirai.bridge.port={socketPort} {Process.StartInfo.Arguments}";
+                socketServer = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socketPort = Config.Instance.bridgePort;
+                IPEndPoint ipEndPoint = new(IPAddress.Loopback, socketPort);
+                try
+                {
+                    socketServer.Bind(ipEndPoint);
+                    onDataReceived($"[通信桥] 端口已绑定到 {socketPort}");
+                }
+                catch (Exception e)
+                {
+                    onDataReceived($"[通信桥] 无法将端口绑定到 {socketPort}");
+                    onDataReceived(e.GetType().Name + ": " + e.Message);
+                    IsRunning = false;
+                    return;
+                }
+                if (!App.CheckBridge())
+                {
+                    onDataReceived($"[通信桥] 无法释放/更新通信桥插件，暂时关闭通信桥");
+                    useBridge = false;
+                }
+            }
             onDataReceived("\u001b[0mmirai-console 正在启动...\u001b[0m");
             onDataReceived("\u001b[0m运行目录: " + Process.StartInfo.WorkingDirectory + "\u001b[0m");
             onDataReceived("\u001b[0m可执行文件: " + Process.StartInfo.FileName + "\u001b[0m");
@@ -98,26 +110,36 @@ namespace GraphicalMirai
             Process.Start();
             WriteLine("status");
             if (task != null) task.Dispose();
-            if (Config.Instance.useBridge && socketServer != null)
+            if (useBridge && socketServer != null)
             {
                 socketServer.Listen(1);
-                taskSocket = new Task(() =>
+                taskSocket = new Task(async () =>
                 {
                     while (!Process.HasExited)
                     {
-                        socket = socketServer.Accept();
-                        while (socket.Connected)
+                        try
                         {
-                            byte[] data = new byte[1024 * 1024 * 1024];
-                            int readLeng = socket.Receive(data, 0, data.Length, SocketFlags.None);
-                            if (readLeng == 0)
+                            onDataReceived("[通信桥] 正在等待连接到 mirai...");
+                            socket = await socketServer.AcceptAsync();
+                            onDataReceived("[通信桥] 已连接到 mirai");
+                            while (socket.Connected)
                             {
-                                socket.Shutdown(SocketShutdown.Both);
-                                socket.Close();
-                                break;
+                                byte[] data = new byte[1024 * 1024 * 1024];
+                                int readLeng = socket.Receive(data, 0, data.Length, SocketFlags.None);
+                                if (readLeng == 0)
+                                {
+                                    socket.Shutdown(SocketShutdown.Both);
+                                    socket.Close();
+                                    break;
+                                }
+                                var msg = Encoding.UTF8.GetString(data, 0, readLeng);
+                                ReceiveRawMessage(msg);
                             }
-                            var msg = Encoding.UTF8.GetString(data, 0, readLeng);
-                            ReceiveRawMessage(msg);
+                            onDataReceived("[通信桥] mirai 已断开连接");
+                        }
+                        catch(Exception e)
+                        {
+                            e.PrintStacktrace();
                         }
                     }
                 });
@@ -144,21 +166,26 @@ namespace GraphicalMirai
                 var botId = @params.SubstringBefore(",").ToLong();
                 var url = @params.SubstringAfter(",");
                 // TODO: 处理滑块验证码
+                onDataReceived($"[通信桥] 滑块验证码请求 {botId}:{url}");
+                return;
             }
+            onDataReceived("[通信桥] mirai>> " + msg);
         }
 
         private void SendRawMessage(string msg) => socket?.Send(Encoding.UTF8.GetBytes(msg));
 
-        public void SliderCaptcha(string botId, string ticket) => SendRawMessage($"SolveSliderCaptcha,{botId},{ticket}");
+        public void SliderCaptcha(string botId, string ticket) 
+        {
+            onDataReceived($"[通信桥] 回应滑块验证码 {botId}:{ticket}");
+            SendRawMessage($"SolveSliderCaptcha,{botId},{ticket}"); 
+        }
 
-        public async void Stop()
+        public void Stop()
         {
             if (!IsRunning || Process.HasExited) return;
             IsRunning = false;
-            WriteLine("stop");
-            await Process.WaitForExitAsync();
-            Process.Close();
-            socketServer?.Shutdown(SocketShutdown.Both);
+            // 虽然可能出现问题，但是要直接杀掉，避免无法执行 stop 的情况下后台跑着进程下一次启动不了
+            Process.Kill();
             socketServer?.Close();
         }
 
