87159684307ba40fb476fc450384b50a535b7010
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-10-09 18:32:50 +0800

    [bridge-plugin] add `/stop` wrapper

--- bridge-plugin/src/main/kotlin/GraphicalMiraiBridge.kt
+++ bridge-plugin/src/main/kotlin/GraphicalMiraiBridge.kt
@@ -1,16 +1,17 @@
 package top.mrxiaom.graphicalmirai
 
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import net.mamoe.mirai.console.command.CommandManager
 import net.mamoe.mirai.console.extension.PluginComponentStorage
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.broadcast
+import okhttp3.internal.closeQuietly
+import top.mrxiaom.graphicalmirai.commands.WrapperedStopCommand
 import top.mrxiaom.graphicalmirai.events.BridgeDataPreReceive
 import java.io.BufferedWriter
 import java.io.DataInputStream
+import java.io.DataInputStream.readUTF
 import java.io.OutputStreamWriter
 import java.io.PrintWriter
 import java.net.Socket
@@ -19,16 +20,17 @@ object GraphicalMiraiBridge : KotlinPlugin(
     JvmPluginDescription(
         id = "top.mrxiaom.graphicalmirai.bridge",
         name = "GraphicalMiraiBridge",
-        version = "0.1.0",
+        version = "0.1.1",
     ) {
         author("MrXiaoM")
         info("""GraphicalMirai 通信桥""")
     }
 ) {
     private lateinit var socket: Socket
-    private lateinit var input: DataInputStream
-    private lateinit var output: PrintWriter
+    private var input: DataInputStream? = null
+    private var output: PrintWriter? = null
     private val sliderDef = CompletableDeferred<String>()
+    private val jobs = mutableListOf<Job>()
     override fun PluginComponentStorage.onLoad() {
         val port = System.getProperty("graphicalmirai.bridge.port")?.toInt()
         if (port == null) {
@@ -36,19 +38,21 @@ object GraphicalMiraiBridge : KotlinPlugin(
             logger.warning("在不使用 GraphicalMirai 时请不要使用该插件")
             return
         }
-        runBlocking(Dispatchers.IO) {
-            try {
-                logger.info("正在连接到 GraphicalMirai (:$port)")
-                socket = Socket("127.0.0.1", port)
-                logger.info(":${socket.localPort} 已连接到 GraphicalMirai (:$port)")
-                output = PrintWriter(BufferedWriter(OutputStreamWriter(socket.getOutputStream())), true)
-                input = DataInputStream(socket.getInputStream())
-                while (socket.isConnected) {
-                    receiveData(input.readUTF())
+        WrapperedStopCommand.hack()
+        try {
+            logger.info("正在连接到 GraphicalMirai (:$port)")
+            socket = Socket("127.0.0.1", port)
+            logger.info(":${socket.localPort} 已连接到 GraphicalMirai (:$port)")
+            output = PrintWriter(BufferedWriter(OutputStreamWriter(socket.getOutputStream())), true)
+            input = DataInputStream(socket.getInputStream())
+            launch(coroutineContext) {
+                while (!socket.isClosed) {
+                    val data = input?.readUTF() ?: continue
+                    receiveData(data)
                 }
-            } catch (t: Throwable) {
-                logger.error(t)
             }
+        } catch (t: Throwable) {
+            logger.error(t)
         }
         contributeBotConfigurationAlterer { _, conf ->
             conf.loginSolver = RemoteLoginSolver
@@ -69,6 +73,7 @@ object GraphicalMiraiBridge : KotlinPlugin(
                 return@launch
             }
         }
+
     }
 
     /**
@@ -78,19 +83,26 @@ object GraphicalMiraiBridge : KotlinPlugin(
      * @return 是否发送成功
      */
     fun sendRawData(data: String): Boolean {
-        if (!this::socket.isInitialized || !this::output.isInitialized) return false
-        output.println(data)
-        return !output.checkError()
+        if (!this::socket.isInitialized || output == null) return false
+        output?.println(data)
+        return !((output?.checkError()) ?: true)
     }
 
     internal suspend fun waitingForTicket(url: String): String {
-        output.println("SolveSliderCaptcha,$url")
+        sendRawData("SolveSliderCaptcha,$url")
         return sliderDef.await()
     }
 
-    override fun onDisable() {
-        if (this::output.isInitialized) output.close()
-        if (this::input.isInitialized) input.close()
-        if (this::socket.isInitialized) socket.close()
+    internal fun disable() {
+        logger.info("停止")
+        try {
+            if (!socket.isClosed) {
+                logger.info("正在断开连接")
+                socket.close()
+                output?.closeQuietly()
+                input?.closeQuietly()
+            }
+        } catch (_: UninitializedPropertyAccessException) {
+        }
     }
 }
---
+++ bridge-plugin/src/main/kotlin/commands/WrapperedStopCommand.kt
@@ -0,0 +1,30 @@
+package top.mrxiaom.graphicalmirai.commands
+
+import net.mamoe.mirai.console.command.BuiltInCommands.StopCommand
+import net.mamoe.mirai.console.command.BuiltInCommands.StopCommand.handle
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.SimpleCommand
+import top.mrxiaom.graphicalmirai.GraphicalMiraiBridge
+
+/**
+ * 不明原因死活不会执行 onDisable
+ * 只好包装一下 /stop
+ */
+object WrapperedStopCommand : SimpleCommand(
+    owner = GraphicalMiraiBridge,
+    primaryName = StopCommand.primaryName,
+    secondaryNames =  StopCommand.secondaryNames,
+    description = StopCommand.description
+) {
+    internal fun hack(){
+        StopCommand.unregister()
+        register()
+    }
+    @Handler
+    suspend fun CommandSender.stop() {
+        GraphicalMiraiBridge.disable()
+        handle()
+    }
+}
