d05d00eabb8277c4ccd708bbf544841f97578bb6
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-10-16 14:44:24 +0800

    Login UI support

--- Pages/PageLogin.xaml.cs
+++ Pages/PageLogin.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media.Media3D;
 
 namespace GraphicalMirai.Pages
 {
@@ -10,6 +11,24 @@ namespace GraphicalMirai.Pages
         public PageLogin()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string qq = textQQ.Text;
+            string password = textPW.Password;
+            App.PageMain.frame.Navigate(App.PageMainConsole);
+            App.mirai.WriteLine($"/login {qq} {password}");
+        }
+
+        private void BtnAutoLogin_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainWindow.Msg.ShowAsync("敬请期待", "未实现");
+        }
+
+        private void BtnDevice_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MainWindow.Msg.ShowAsync("敬请期待", "未实现");
         }
     }
 }
